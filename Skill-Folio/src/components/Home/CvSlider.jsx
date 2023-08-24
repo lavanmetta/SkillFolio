70bc65e21515ec5fbe770ce0dba2470d0b4f0c81
lavanmetta <lavanmetta111@gmail.com>
2023-08-25 00:31:00 +0530

    compketed resume pics

--- Skill-Folio/src/components/Home/CvSlider.jsx
+++ Skill-Folio/src/components/Home/CvSlider.jsx
@@ -3,7 +3,7 @@ import { ListOfImages } from "../../Data/pics";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useState } from "react";
-
+import { Image, Shimmer } from "react-shimmer";
 export default function CvSlider() {
   const [ListOfResumes, setListOfResumes] = useState([]);
 
@@ -16,10 +16,10 @@ export default function CvSlider() {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 2000,
+    speed: 3000,
     autoplaySpeed: 3000,
     cssEase: "linear",
-    pauseOnHover: false
+    pauseOnHover: false,
   };
 
   useEffect(() => {
@@ -31,7 +31,11 @@ export default function CvSlider() {
       <Slider {...settings} className="slider">
         {ListOfResumes.map((each, index) => (
           <div key={index} className="resume-container">
-            <img src={each} className="resume-img" />
+            <Image
+              src={each}
+              className="resume-img"
+              fallback={<Shimmer width={250} height={350} />}
+            />
           </div>
         ))}
       </Slider>
