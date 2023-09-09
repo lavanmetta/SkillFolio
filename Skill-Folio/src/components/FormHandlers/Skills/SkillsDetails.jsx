aa0eb26872cf2833669d728e233d3d2a418a65b6
lavanmetta <lavanmetta111@gmail.com>
2023-09-09 20:30:08 +0530

    developing all forms

---
+++ Skill-Folio/src/components/FormHandlers/Skills/SkillsDetails.jsx
@@ -0,0 +1,9 @@
+
+
+export default function SkillsDetails() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- Skill-Folio/src/components/Resume/FormSection.jsx
+++ Skill-Folio/src/components/Resume/FormSection.jsx
@@ -1,11 +1,13 @@
 import BasicInfo from "../FormHandlers/BasicInfo/BasicInfo";
 import AddEducation from "../FormHandlers/Education/AddEducation";
+import SkillsDetails from "../FormHandlers/Skills/SkillsDetails";
 import "./FormSection.css";
 export default function FormSection() {
   return (
     <div className="formsection-container">
       <BasicInfo />
       <AddEducation />
+      <SkillsDetails />
     </div>
   );
 }
